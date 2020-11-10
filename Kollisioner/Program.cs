609e3242c0d6b04234726a6fc343c5d94345553e
Mikael <mikael.bergstrom@ga.ntig.se>
2020-11-10 10:35:38 +0100

    Initial commit

---
+++ Kollisioner/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Kollisioner
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Raylib.InitWindow(800, 600, "Kollisioner");
+      Raylib.SetTargetFPS(60);
+
+      Rectangle first = new Rectangle(10, 10, 50, 50);
+
+      Rectangle second = new Rectangle(35, 35, 50, 50);
+
+      Color semiTransparent = new Color(255, 0, 0, 128);
+
+
+      while (!Raylib.WindowShouldClose())
+      {
+
+        first.x += 0.5f;
+
+
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(Color.SKYBLUE);
+
+        Raylib.DrawRectangleRec(first, semiTransparent);
+        Raylib.DrawRectangleRec(second, semiTransparent);
+
+        if (Raylib.CheckCollisionRecs(first, second))
+        {
+          Raylib.DrawText("YES", 100, 100, 64, Color.BLACK);
+        }
+
+        Raylib.EndDrawing();
+      }
+
+    }
+  }
+}
